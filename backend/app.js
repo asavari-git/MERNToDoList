601b6e7ccdba981ff6ce99361804022f9bbdde0e
asavari-git <asavari.kurhekar8@gmail.com>
2024-09-10 06:48:13 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,12 @@ const auth = require("./routes/auth");
 const list = require("./routes/list");
 
 app.use(express.json());
-app.use(cors({origin5: "*"}));
+app.use(cors({origin: "*"}));
+// app.use(cors({
+//     origin: 'https://merntodolist-frontend.onrender.com', // Your frontend domain
+//     methods: 'GET,POST,PUT,DELETE', // Allowed methods
+//     credentials: true // If you need to send cookies or authorization headers
+// }));
 
 app.get("/", (req, res) =>{
     res.send("Hello");
